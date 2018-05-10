7cac8c1c447c3c6b55a3b5e6b17d3704caefcc6d
Justin Swaney <jswaney@mit.edu>
2018-05-10 12:45:26 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = "0.1.0"
+
+with open("./README.md") as fd:
+    long_description = fd.read()
+
+setup(
+    name="pystripe",
+    version=version,
+    description=
+    "Stripe artifact filtering for SPIM images",
+    long_description=long_description,
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-image",
+        "PyWavelets",
+        "tqdm",
+        "scikit-learn"
+    ],
+    author="Kwanghun Chung Lab",
+    packages=["pystripe"],
+    # entry_points={ 'console_scripts': [
+    #     'phathom-segmentation=phathom.segmentation:main',
+    #     'phathom-score-centroids=phathom.score.main'
+    # ]},
+    url="https://github.com/chunglabmit/pystripe",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python :: 3.6',
+    ]
+)
